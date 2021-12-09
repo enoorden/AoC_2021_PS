087a4e2fdefe253eee409c078731bb3eddef9c6c
Erik van Noorden <erik.vannoorden@ogd.nl>
2021-12-09 23:39:08 +0100

    day9 solved

--- 09_code.ps1
+++ 09_code.ps1
@@ -6,7 +6,7 @@ $orgData = (Get-Content .\$fn) | ForEach-Object { , @($_ -split '' | Where-Objec
 $horData = (Get-Content .\$fn) | ForEach-Object { , @($_ -split '' | Where-Object { $_ }) }
 $vertData = (Get-Content .\$fn) | ForEach-Object { , @($_ -split '' | Where-Object { $_ }) }
 
-function checkHights ($inArr) {
+function checkHeightsHorizontal ($inArr) {
     1..2 | ForEach-Object {
         $inArr | ForEach-Object { [array]::Reverse($_) }
         for ($y = 0; $y -lt $inArr.Length; $y++) {
@@ -19,37 +19,38 @@ function checkHights ($inArr) {
     $inArr
 }
 
-function rotArr ($inArr) {
-    $rotated = @()
-    for ($i = 0; $i -lt $inArr[0].Length; $i++) {
-        $rotated += , @($inArr | ForEach-Object { $_[$i] | Where-Object { $_ } })
+function checkHeightsVertical ($inArr) {
+    1..2 | ForEach-Object {
+        [array]::Reverse($inArr)
+        for ($x = 0; $x -lt ($inArr[0].Length); $x++) {
+
+            for ($y = 0; $y -lt ($inArr.Length - 1); $y++) {
+                if ($inArr[$y][$x] -eq 9) { continue }
+                if ($inArr[$y][$x] -gt $inArr[$y + 1][$x]) { $inArr[$y][$x] = 9 }
+            }
+        }
     }
-    $rotated
+    $inArr
 }
 
-#horizontal pass
-$horData = checkHights $horData
-
-#vertical pass
-$rotated = checkHights (rotArr $vertData)
-$rotatedBack = rotArr $rotated
+$horData = checkHeightsHorizontal $horData
+$vertData = checkHeightsVertical $vertData
 
 #compare
 $total = for ($y = 0; $y -lt $horData.Length; $y++) {
     for ($x = 0; $x -lt ($horData[0].Length); $x++) {
         if ($horData[$y][$x] -eq 9) { continue }
-        if ($horData[$y][$x] -eq $rotatedBack[$y][$x]) { [int]$horData[$y][$x] + 1 }
+        if ($horData[$y][$x] -eq $vertData[$y][$x]) { [int]$horData[$y][$x] + 1 }
     }
 }
 
 Write-Host 'Part 1 :' ($total | Measure-Object -Sum).Sum
 
-
 #PArt2
 $lows = for ($y = 0; $y -lt $horData.Length; $y++) {
     for ($x = 0; $x -lt ($horData[0].Length); $x++) {
         if ($horData[$y][$x] -eq 9) { continue }
-        if ($horData[$y][$x] -eq $rotatedBack[$y][$x]) { , @($y, $x) }
+        if ($horData[$y][$x] -eq $vertData[$y][$x]) { , @($y, $x) }
     }
 }
 
@@ -66,15 +67,19 @@ do {
             if ($orgData[$y][$x] -like 'B*') { continue }
 
             if ($y -gt 0) { if ($orgData[$y - 1][$x] -like 'B*') { $orgData[$y][$x] = $orgData[$y - 1][$x] } }
-            if ($y -lt $orgData.Length -1) { if ($orgData[$y + 1][$x] -like 'B*') { $orgData[$y][$x] = $orgData[$y + 1][$x] } }
+            if ($y -lt $orgData.Length - 1) { if ($orgData[$y + 1][$x] -like 'B*') { $orgData[$y][$x] = $orgData[$y + 1][$x] } }
             if ($x -gt 0) { if ($orgData[$y][$x - 1] -like 'B*') { $orgData[$y][$x] = $orgData[$y][$x - 1] } }
             if ($x -lt $orgData[0].Length) { if ($orgData[$y][$x + 1] -like 'B*') { $orgData[$y][$x] = $orgData[$y][$x + 1] } }
         }
     }
 } while ($orgdata | ForEach-Object { $_ | Where-Object { $_ -notlike 'B*' -and $_ -ne 9 } })
 
-(($orgData | ForEach-Object { $_ | Where-Object { $_ -ne 9 } }) | Group-Object | Sort-Object count -Descending -Top 3)
 
+$total = 1
+((($orgData | ForEach-Object { $_ | Where-Object { $_ -ne 9 } }) | Group-Object | Sort-Object count -Descending -Top 3) | Select-Object -expand count) | ForEach-Object {
+    $total *= $_
+}
+Write-Host "Part 2: $total"
 
 $stopwatch.Stop()
 Write-Host "Time Elapsed: $($Stopwatch.Elapsed.ToString())"
