292daff762df8bb22a9204a1dda8e69277055040
= <erikn@grrr.be>
2021-12-11 20:42:30 +0100

    faster

--- 11_code.ps1
+++ 11_code.ps1
@@ -1,52 +1,70 @@
+$stopwatch = [System.Diagnostics.Stopwatch]::new()
+$Stopwatch.Start()
+
+Write-Host 'Day10'
+
 if ($data) { Remove-Variable data }
 $data = (Get-Content .\11_input.txt) | ForEach-Object { , [int[]]@($_ -split '' | Where-Object { $_ }) }
-$yy = $data.Length - 1
-$xx = $data[0].Length - 1
 
-function chargeAround($data, $pos) {
+$xx = $data[0].Length
+$yy = $data.Length
+
+function Flash($data, $pos) {
     if ($data[$pos[0]][$pos[1]] -eq 0) { return }
+
     $data[$pos[0]][$pos[1]] = 0
-    ($pos[0] - 1)..($pos[0] + 1) | ForEach-Object {
-        $y = $_
-        ($pos[1] - 1)..($pos[1] + 1) | ForEach-Object {
-            $x = $_
-            if ($x -lt 0 -or $x -gt $xx) { return }
-            if ($y -lt 0 -or $y -gt $yy) { return }
-            if ($data[$y][$x] -eq 0) { return }
+
+    for ($y = $pos[0] - 1; $y -le $pos[0] + 1; $y++) {
+        for ($x = $pos[1] - 1; $x -le $pos[1] + 1; $x++) {
+            if ($x -lt 0 -or $x -gt ($xx - 1)) { continue }
+            if ($y -lt 0 -or $y -gt ($yy - 1)) { continue }
+            if ($data[$y][$x] -eq 0) { continue }
 
             $data[$y][$x] += 1
 
-            if ($data[$y][$x] -gt 9) { chargeAround $data @($y, $x) }
+            if ($data[$y][$x] -gt 9) { Flash $data @($y, $x) }
         }
     }
 }
 
+function stillCharged {
+    for ($y = 0; $y -lt $yy; $y++) {
+        for ($x = 0; $x -lt $xx; $x++) {
+            if ($data[$y][$x] -gt 9) { return $true }
+        }
+    }
+    return $false
+}
+
 $flashes = 0
 
-#get positions which should flash
 for ($step = 1; $step -le 1000000000; $step++) {
     #increment
-    for ($y = 0; $y -lt $data.Length; $y++) {
-        for ($x = 0; $x -lt ($data[0].Length); $x++) {
+    for ($y = 0; $y -lt $yy; $y++) {
+        for ($x = 0; $x -lt $xx; $x++) {
             [int]$data[$y][$x] += 1
         }
     }
 
     #get charged
-    while ( $data | ForEach-Object { $_ | Where-Object { $_ -gt 9 } }) {
-        $charged = for ($y = 0; $y -lt $data.Length; $y++) {
-            for ($x = 0; $x -lt $data[0].Length; $x++) {
+    while ( stillCharged ) {
+        $charged = for ($y = 0; $y -lt $yy; $y++) {
+            for ($x = 0; $x -lt $xx; $x++) {
                 if ($data[$y][$x] -gt 9) { , @($y, $x) }
             }
         }
         if ($charged[0] -is [int]) { $charged = , @($charged[0], $charged[1]) }
 
-        $charged | ForEach-Object { chargeAround $data $_ }
+        #flash
+        $charged | ForEach-Object { Flash $data $_ }
     }
 
     $stepFlash = ($data | ForEach-Object { $_ | Where-Object { $_ -eq 0 } }).count
     $flashes += $stepFlash
 
-    if ($step -eq 100) { Write-Host "Part 1: $flashes" }
+    if ($step -eq 100) { Write-Host "Part 1: $flashes"; Write-Host "Time Elapsed: $($Stopwatch.Elapsed.ToString())" }
     if ($stepFlash -eq 100) { Write-Host "Part 2: $step"; break }
 }
+
+$stopwatch.Stop()
+Write-Host "Time Elapsed: $($Stopwatch.Elapsed.ToString())"
